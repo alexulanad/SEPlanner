74cae0062bd1d69907c0e94aa1bde014c65b1eef
AlexuL <alexulanad@gmail.com>
2022-05-25 15:24:32 +0300

    Добавлена функция назначения и удаления классов. Hover и Focus блока категорий настроены с использованием данной функции.

--- js/main.js
+++ js/main.js
@@ -6,74 +6,122 @@
 // });
 
 // sizeSelectionCategory
-let largeBlockCategory = true;
-let LargeBlockProject = true;
-const largeBlockCategoryView = document.querySelector('#large-block-category');
-const smallBlocksCategoryView = document.querySelector('#small-blocks-category');
-const categories = document.querySelector('#categories');
-const categoryName = document.querySelector('#category-name');
-console.dir(categoryName);
+let largeBlockCategoryActive = true;
+let LargeBlockProjectActive = true;
+const largeBlockCategory = document.querySelector('#large-block-category');
+const smallBlocksCategory = document.querySelector('#small-blocks-category');
+let categories = document.querySelector('#categories');
+let categoryTitle = document.querySelector('#category-title');
 
-// console.log(categories);
+// for (let item of categories.children) {
+//     if (item.tagName === "IMG") {
+//         item.addEventListener("mouseover", (e, categoryName)=> {
+//             categoryName.textContent = e.dataset.categoryTitle;
+//         });
+//         item.addEventListener("mouseout", ()=> {
+
+//         });
+//         item.addEventListener("click", ()=> {
+
+//         } );
+//     }
+// }
+
+const addClass = function(event, className) {
+    if (event.target) {
+        event.target.classList.add(className);
+    } else {
+        event.classList.add(className);
+    }
+};
+
+const removeClass = function(event, className) {
+    if (event.target) {
+        event.target.classList.remove(className);
+    } else {
+        event.classList.remove(className);
+    }
+};
+
+// for (let item of categories.children) {
+// if (item.dataset.active === "true") {
+//     console.log(item.dataset.active);
+//     addClass(item, "block-image--focus");
+// } else {
+//     console.log(item.dataset.active);
+//     removeClass(item, "block-image--focus");
+// }
+// }
+// addClass(categories.firstElementChild, "block-image--focus");
 
 for (let item of categories.children) {
+
+
     if (item.tagName === "IMG") {
-        item.addEventListener("mouseover", ()=> {
+        item.addEventListener("mouseover", (event) => {
+            addClass(event, "block-image--hover");
+            categoryTitle.textContent = item.dataset.categoryName;
+        }, false);
+        item.addEventListener("mouseout", (event) => removeClass(event, "block-image--hover"), false);
+        item.addEventListener("click", () => {
+            item.dataset.active = "true";
+            addClass(item, "block-image--focus");
 
+            const categoryName = item.dataset.categoryName;
+            for (let item of categories.children) {
+                if (item.dataset.categoryName != categoryName) {
+                    item.dataset.active = "false";
+                    removeClass(item, "block-image--focus");
+                }
+            }
         });
-        item.addEventListener("mouseout", ()=> {
-
-        });
-        item.addEventListener("click", ()=> {
-
-        } );
     }
 }
 
-largeBlockCategoryView.addEventListener("mouseover", ()=> {
-    largeBlockCategoryView.classList.add('size-selection__large-block--hover');
+largeBlockCategory.addEventListener("mouseover", ()=> {
+    largeBlockCategory.classList.add('size-selection__large-block--hover');
 });
 
-largeBlockCategoryView.addEventListener("mouseout", ()=> {
-    largeBlockCategoryView.classList.remove('size-selection__large-block--hover');
+largeBlockCategory.addEventListener("mouseout", ()=> {
+    largeBlockCategory.classList.remove('size-selection__large-block--hover');
 });
 
-largeBlockCategoryView.addEventListener("click", ()=> {
-    largeBlockCategory = true;
-    largeBlockCategoryView.classList.add('size-selection__large-block--focus');
-    for (let item of smallBlocksCategoryView.children) {
+largeBlockCategory.addEventListener("click", ()=> {
+    largeBlockCategoryActive = true;
+    largeBlockCategory.classList.add('size-selection__large-block--focus');
+    for (let item of smallBlocksCategory.children) {
         if (item.classList.contains("size-selection__small-block")) {
             item.classList.remove('size-selection__small-block--focus');
         }
     }
-    console.log(largeBlockCategory);
+    console.log(largeBlockCategoryActive);
 });
 
-smallBlocksCategoryView.addEventListener("mouseover", ()=> {
-    for (let item of smallBlocksCategoryView.children) {
+smallBlocksCategory.addEventListener("mouseover", ()=> {
+    for (let item of smallBlocksCategory.children) {
         if (item.classList.contains("size-selection__small-block")) {
             item.classList.add('size-selection__small-block--hover');
         }
     }
 });
 
-smallBlocksCategoryView.addEventListener("mouseout", ()=> {
-    for (let item of smallBlocksCategoryView.children) {
+smallBlocksCategory.addEventListener("mouseout", ()=> {
+    for (let item of smallBlocksCategory.children) {
         if (item.classList.contains("size-selection__small-block")) {
             item.classList.remove('size-selection__small-block--hover');
         }
     }
 });
 
-smallBlocksCategoryView.addEventListener("click", ()=> {
-    largeBlockCategory = false;
-    largeBlockCategoryView.classList.remove('size-selection__large-block--focus');
-    for (let item of smallBlocksCategoryView.children) {
+smallBlocksCategory.addEventListener("click", ()=> {
+    largeBlockCategoryActive = false;
+    largeBlockCategory.classList.remove('size-selection__large-block--focus');
+    for (let item of smallBlocksCategory.children) {
         if (item.classList.contains("size-selection__small-block")) {
             item.classList.add('size-selection__small-block--focus');
         }
     }
-    console.log(largeBlockCategory);
+    console.log(largeBlockCategoryActive);
 });
 
 
