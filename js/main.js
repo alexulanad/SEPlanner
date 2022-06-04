89ff552161ec21e2d80ab3b08049c14e6e3592fa
AlexuL <alexulanad@gmail.com>
2022-06-04 15:28:46 +0300

    Завершена настройка кнопки добавления блоков и взаимодействие с ней

--- js/main.js
+++ js/main.js
@@ -50,9 +50,11 @@ const displayBlocksCategory = function(categoryKey) {
                 <img class="block-image--ss" src="img/blocks/${item.img}">
                 <span class="content-block__block-item-name">${item.title.ru}</span>
                 <div class="button-add-block">
-                    <svg class="button-add-block__svg" width="28" height="28" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path class="button-add-block__path" d="M12 2C11.4477 2 11 2.44772 11 3V5C11 5.55228 11.4477 6 12 6H15C15.5523 6 16 5.55228 16 5V3C16 2.44772 15.5523 2 15 2H12ZM14 7.5C14 7.22386 13.7761 7 13.5 7C13.2239 7 13 7.22386 13 7.5V13.2929L11.8536 12.1464C11.6583 11.9512 11.3417 11.9512 11.1464 12.1464C10.9512 12.3417 10.9512 12.6583 11.1464 12.8536L13.1464 14.8536C13.3417 15.0488 13.6583 15.0488 13.8536 14.8536L15.8536 12.8536C16.0488 12.6583 16.0488 12.3417 15.8536 12.1464C15.6583 11.9512 15.3417 11.9512 15.1464 12.1464L14 13.2929V7.5ZM5 11C4.44772 11 4 11.4477 4 12V14C4 14.5523 4.44772 15 5 15H8C8.55229 15 9 14.5523 9 14V12C9 11.4477 8.55229 11 8 11H5ZM3 13.5C3 13.2239 2.77614 13 2.5 13C2.22386 13 2 13.2239 2 13.5V15C2 16.1046 2.89543 17 4 17H16C17.1046 17 18 16.1046 18 15V13.5C18 13.2239 17.7761 13 17.5 13C17.2239 13 17 13.2239 17 13.5V15C17 15.5523 16.5523 16 16 16H4C3.44772 16 3 15.5523 3 15V13.5Z" fill="#D0D0D0"/>
-                    </svg>
+                <svg class="button-add-block__svg" width="26" height="26" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path class="button-add-block__path" d="M19 15V12H17V15H14V17H17V20H19V17H22V15H19ZM2 7H15V9H2V7ZM2 11H15V13H2V11ZM2 15H12V17H2V15Z" fill="#AEC2CC"/>
+                </svg>
+
+
                 </div>
             </div>
             `;
@@ -69,12 +71,12 @@ const displayBlocksCategory = function(categoryKey) {
 
 
              item.addEventListener("mouseover", () => {
-                addBlock.style.visibility = "visible";
+                // addBlock.style.visibility = "visible";
                  console.log(item.dataset.blockId);
              });
 
              item.addEventListener("mouseout", () => {
-                addBlock.style.visibility = "hidden";
+                // addBlock.style.visibility = "hidden";
                  console.log(item.dataset.blockId);
              });
 
