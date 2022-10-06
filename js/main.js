57867cb840f94160f919e531f935d0da0cbaf037
AlexuL <alexulanad@gmail.com>
2022-10-06 10:43:37 +0300

    События категорий разделены на отдельные методы класса, вызываются основным методом назначения событий этого класса

--- js/main.js
+++ js/main.js
@@ -158,7 +158,6 @@ const blockSizeSwitchProject = {
 class CategoryBlocks {
     constructor() {
         this.blockList = document.querySelector('#block-list-category');
-        // this.listType = "category";
         this.blocks = main.largeBlockCategoryActive == true ? blocks[main.categoryKeyTarget].large : blocks[main.categoryKeyTarget].small;
         this.dropDownBlock = main.largeBlockCategoryActive === true ? main.categoryLargeBlock : main.categorySmallBlock;
     }
@@ -242,23 +241,6 @@ class CategoryBlocks {
             const blockItemBase = item.querySelector(".block-item__base");
             const specification = item.querySelector(".block-item__specification");
 
-            // blockItemBase.addEventListener("mousedown", (e)=> {
-            //     if (e.currentTarget == e.target) {
-            //         // Раскрывает дополнительную информацию о блоке, сворачивает предыдущий раскрытый блок, сворачивает раскрытый блок, сохраняет индекс раскрытого блока
-            //         if (this.returnBlockIndex().index === null) {
-            //             this.returnBlockIndex().index = specification.dataset.blockId;
-            //             $lib.dropDown(specification, 0.2);
-            //         } else if (this.returnBlockIndex().index !== null && this.returnBlockIndex().index === specification.dataset.blockId) {
-            //             this.returnBlockIndex().index = null;
-            //             $lib.dropDown(specification, 0.2);
-            //         } else if (this.returnBlockIndex().index !== null && this.returnBlockIndex().index !== specification.dataset.blockId) {
-            //             $lib.dropDown(this.blockList.querySelector(`[data-block-id="${this.returnBlockIndex().index}"]`), 0.2);
-            //             this.returnBlockIndex().index = specification.dataset.blockId;
-            //             $lib.dropDown(specification, 0.2);
-            //         }
-            //     }
-            // });
-
             blockItemBase.addEventListener("mousedown", (e)=> {
                 if (e.currentTarget == e.target) {
                     // Раскрывает дополнительную информацию о блоке, сворачивает предыдущий раскрытый блок, сворачивает раскрытый блок, сохраняет индекс раскрытого блока
@@ -284,156 +266,161 @@ class CategoryBlocks {
                 buttonIconSvg.classList.add("button-icon__svg--click");
             });
 
-            //Добавляет блок в массив блоков проекта, запускает метод отображения блоков данного массива из списка блоков проекта
-            buttonIcon.addEventListener("mouseup", () => {
-                buttonIconSvg.classList.remove("button-icon__svg--click");
-                const createProjectBlock = new CreateProjectBlock(this.blocks[specification.dataset.blockId], (main.largeBlockCategoryActive === true) ? true : false, 1, false);
-                main.projectBlocks.push(createProjectBlock);
-                projectBlocks.displayBlocks();
-            });
+            this.addEventButton(buttonIcon, buttonIconSvg, specification);
         }
     }
-    // Возвращает индекс блока, учитывая назначение списка блоков, а так же активный переключатель размера блоков
-    // returnBlockIndex() {
-    //     if (this.listType === "category" && main.largeBlockCategoryActive === true) {return main.categoryLargeBlock;}
-    //     else if (this.listType === "category" && main.largeBlockCategoryActive === false) {return main.categorySmallBlock;}
-    //     else if (this.listType === "project" && main.largeBlockProjectActive === true) {return main.projectLargeBlock;}
-    //     else if (this.listType === "project" && main.largeBlockProjectActive === false) {return main.projectSmallBlock;}
-    // }
+    //Добавляет блок в массив блоков проекта, запускает метод отображения блоков данного массива из списка блоков проекта
+    addEventButton(buttonIcon, buttonIconSvg, specification) {
+        buttonIcon.addEventListener("mouseup", () => {
+            buttonIconSvg.classList.remove("button-icon__svg--click");
+            const createProjectBlock = new CreateProjectBlock(this.blocks[specification.dataset.blockId], (main.largeBlockCategoryActive === true) ? true : false, 1, false);
+            main.projectBlocks.push(createProjectBlock);
+            projectBlocks.displayBlocks();
+        });
+    }
 }
 
-const projectBlocks = {
-    blockList: document.querySelector('#block-list-project'),
+class ProjectBlocks extends CategoryBlocks{
+    constructor() {
+        super();
+        this.blockList = document.querySelector('#block-list-project');
+        this.blocks = main.largeBlockProjectActive == true ? blocks[main.categoryKeyTarget].large : blocks[main.categoryKeyTarget].small;
+        this.dropDownBlock = main.largeBlockCategoryActive === true ? main.categoryLargeBlock : main.categorySmallBlock;
+    }
+}
 
-    // Функция сортировки блоков проекта из массива projectBlocks, по критерию: большие, малые, и дальнейший вызов функции отображения блоков
-    displayBlocks() {
-        this.blockList.innerHTML = "";
-        if (main.projectBlocks.length != 0) {
-            if (main.largeBlockProjectActive === true) {
-                main.projectBlocks.filter(function (item, index) {
-                    if (item.largeBlock === true) {
-                        projectBlocks.addBlock(item, index);
-                    }
-                });
-            } else {
-                main.projectBlocks.filter(function (item, index) {
-                    if (item.largeBlock !== true) {
-                        projectBlocks.addBlock(item, index);
-                    }
-                });
-            }
-        }
-        // Удаляем лишний нижний отступ у последнего отображенного блока
-        if (this.blockList.innerHTML != "") {
-            this.blockList.lastElementChild.style.marginBottom = 0;
-        }
-        this.addEvent();
-        this.scrollCheck(this.blockList);
-    },
+// const projectBlocks = {
+//     blockList: document.querySelector('#block-list-project'),
 
-    // функция добавления и вывода блоков проекта на экран
-    addBlock(item, index) {
-        this.blockList.innerHTML += `
-        <div class="block-item">
-            <div class="block-item__base">
-                <img class="block-image--ss" src="img/blocks/${item.block.img}">
-                <span class="block-item__name">${item.block.title.ru}</span>
-                <span class="block-item__name">${item.amount}</span>
-                <div class="button-icon" data-block-id="${index}">
-                    <svg class="button-icon__svg" width="26" height="26" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-playlist-remove">
-                        <path class="button-icon__svg-path" d="M2,6V8H14V6H2M2,10V12H11V10H2M14.17,10.76L12.76,12.17L15.59,15L12.76,17.83L14.17,19.24L17,16.41L19.83,19.24L21.24,17.83L18.41,15L21.24,12.17L19.83,10.76L17,13.59L14.17,10.76M2,14V16H11V14H2Z" fill="rgb(174, 194, 204, 0.9)"/>
-                    </svg>
-                </div>
-            </div>
-            <div class="block-item__specification">
-                <div class="block-item__image-block">
-                    <img class="block-image--bs" src="img/blocks/${item.block.img}">
-                </div>
-                <div class="block-item__specification-block">
-                    <div class="block-item__content-block">
-                        <div class="block-item__content-title">Описание</div>
-                        <div class="block-item__content-description">${item.block.description}</div>
-                    </div>
+//     // Функция сортировки блоков проекта из массива projectBlocks, по критерию: большие, малые, и дальнейший вызов функции отображения блоков
+//     displayBlocks() {
+//         this.blockList.innerHTML = "";
+//         if (main.projectBlocks.length != 0) {
+//             if (main.largeBlockProjectActive === true) {
+//                 main.projectBlocks.filter(function (item, index) {
+//                     if (item.largeBlock === true) {
+//                         projectBlocks.addBlock(item, index);
+//                     }
+//                 });
+//             } else {
+//                 main.projectBlocks.filter(function (item, index) {
+//                     if (item.largeBlock !== true) {
+//                         projectBlocks.addBlock(item, index);
+//                     }
+//                 });
+//             }
+//         }
+//         // Удаляем лишний нижний отступ у последнего отображенного блока
+//         if (this.blockList.innerHTML != "") {
+//             this.blockList.lastElementChild.style.marginBottom = 0;
+//         }
+//         this.addEvent();
+//         this.scrollCheck(this.blockList);
+//     },
 
-                    <div class="block-item__content-block">
-                        <div class="block-item__content-title">Характеристики</div>
-                        <div class="block-item__content-item">
-                            <span class="block-item__content-item-title">Масса</span>
-                            <span class="block-item__content-item-value">${item.block.weight}</span>
-                            <span class="block-item__content-item-value-unit">кг.</span>
-                        </div>
-                        <div class="block-item__content-item">
-                            <span class="block-item__content-item-title">Прочность</span>
-                            <span class="block-item__content-item-value">${item.block.integrity}</span>
-                            <span class="block-item__content-item-value-unit">ед.</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        `;
-        document.querySelector('#block-list-project').scrollTop = 0;
-    },
-    // функция добавления событий для блоков проекта
-    addEvent() {
-        for (let item of this.blockList.children) {
-            const buttonIcon = item.querySelector(".button-icon");
-            const buttonIconSvg = item.querySelector(".button-icon__svg");
-            const blockItemBase = item.querySelector(".block-item__base");
-            const blockItemSpecification = item.querySelector(".block-item__specification");
+//     // функция добавления и вывода блоков проекта на экран
+//     addBlock(item, index) {
+//         this.blockList.innerHTML += `
+//         <div class="block-item">
+//             <div class="block-item__base">
+//                 <img class="block-image--ss" src="img/blocks/${item.block.img}">
+//                 <span class="block-item__name">${item.block.title.ru}</span>
+//                 <span class="block-item__name">${item.amount}</span>
+//                 <div class="button-icon" data-block-id="${index}">
+//                     <svg class="button-icon__svg" width="26" height="26" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-playlist-remove">
+//                         <path class="button-icon__svg-path" d="M2,6V8H14V6H2M2,10V12H11V10H2M14.17,10.76L12.76,12.17L15.59,15L12.76,17.83L14.17,19.24L17,16.41L19.83,19.24L21.24,17.83L18.41,15L21.24,12.17L19.83,10.76L17,13.59L14.17,10.76M2,14V16H11V14H2Z" fill="rgb(174, 194, 204, 0.9)"/>
+//                     </svg>
+//                 </div>
+//             </div>
+//             <div class="block-item__specification">
+//                 <div class="block-item__image-block">
+//                     <img class="block-image--bs" src="img/blocks/${item.block.img}">
+//                 </div>
+//                 <div class="block-item__specification-block">
+//                     <div class="block-item__content-block">
+//                         <div class="block-item__content-title">Описание</div>
+//                         <div class="block-item__content-description">${item.block.description}</div>
+//                     </div>
 
-            buttonIcon.addEventListener("mouseleave", () => {
-                buttonIconSvg.classList.remove("button-icon__svg--click");
-            });
+//                     <div class="block-item__content-block">
+//                         <div class="block-item__content-title">Характеристики</div>
+//                         <div class="block-item__content-item">
+//                             <span class="block-item__content-item-title">Масса</span>
+//                             <span class="block-item__content-item-value">${item.block.weight}</span>
+//                             <span class="block-item__content-item-value-unit">кг.</span>
+//                         </div>
+//                         <div class="block-item__content-item">
+//                             <span class="block-item__content-item-title">Прочность</span>
+//                             <span class="block-item__content-item-value">${item.block.integrity}</span>
+//                             <span class="block-item__content-item-value-unit">ед.</span>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//         `;
+//         document.querySelector('#block-list-project').scrollTop = 0;
+//     },
+//     // функция добавления событий для блоков проекта
+//     addEvent() {
+//         for (let item of this.blockList.children) {
+//             const buttonIcon = item.querySelector(".button-icon");
+//             const buttonIconSvg = item.querySelector(".button-icon__svg");
+//             const blockItemBase = item.querySelector(".block-item__base");
+//             const blockItemSpecification = item.querySelector(".block-item__specification");
 
-            buttonIcon.addEventListener("mousedown", ()=> {
-                buttonIconSvg.classList.add("button-icon__svg--click");
-            });
+//             buttonIcon.addEventListener("mouseleave", () => {
+//                 buttonIconSvg.classList.remove("button-icon__svg--click");
+//             });
 
-            buttonIcon.addEventListener("mouseup", (event)=> {
-                buttonIconSvg.classList.remove("button-icon__svg--click");
-                main.projectBlocks.splice(event.currentTarget.dataset.blockId, 1);
-                this.displayBlocks();
-            });
+//             buttonIcon.addEventListener("mousedown", ()=> {
+//                 buttonIconSvg.classList.add("button-icon__svg--click");
+//             });
 
-            blockItemBase.addEventListener("mousedown", (e)=> {
-                if (e.currentTarget == e.target) {
-                    if (blockItemSpecification.style.maxHeight) {
-                        blockItemSpecification.style.transition = "max-height 0.2s ease-out";
-                        blockItemSpecification.style.maxHeight = null;
-                        main.projectBlocks[buttonIcon.dataset.blockId].active = false;
-                    } else {
-                        for (let item of this.blockList.children) {
-                            const blockItemSpecification = item.querySelector(".block-item__specification");
-                            const buttonIcon = item.querySelector(".button-icon");
-                            if (blockItemSpecification.style.maxHeight) {
-                                blockItemSpecification.style.transition = "max-height 0.2s ease-out";
-                                blockItemSpecification.style.maxHeight = null;
-                                main.projectBlocks[buttonIcon.dataset.blockId].active = false;
-                            }
-                        }
-                        blockItemSpecification.style.transition = "max-height 0.2s ease-out";
-                        main.projectBlocks[buttonIcon.dataset.blockId].active = true;
-                        blockItemSpecification.style.maxHeight = blockItemSpecification.scrollHeight + "px";
-                    }
-                }
-            }, true);
+//             buttonIcon.addEventListener("mouseup", (event)=> {
+//                 buttonIconSvg.classList.remove("button-icon__svg--click");
+//                 main.projectBlocks.splice(event.currentTarget.dataset.blockId, 1);
+//                 this.displayBlocks();
+//             });
 
-            if (main.projectBlocks[buttonIcon.dataset.blockId].active === true) {
-                blockItemSpecification.style.transition = "none";
-                blockItemSpecification.style.maxHeight = blockItemSpecification.scrollHeight + "px";
-            }
-        }
-    },
-    // Метод добавляет отступ справа для каждого игрового блока, при условии появления скролла
-    scrollCheck(blockList) {
-        // проверяем реальную ширину offset* элемента и фактическую client* (без учета ширины scroll-а)
-        if (blockList.offsetWidth > blockList.clientWidth) {
-            blockList.querySelectorAll('.block-item').forEach(item => {
-                item.style.marginRight = "4px";
-            });
-        }
-    },
-};
+//             blockItemBase.addEventListener("mousedown", (e)=> {
+//                 if (e.currentTarget == e.target) {
+//                     if (blockItemSpecification.style.maxHeight) {
+//                         blockItemSpecification.style.transition = "max-height 0.2s ease-out";
+//                         blockItemSpecification.style.maxHeight = null;
+//                         main.projectBlocks[buttonIcon.dataset.blockId].active = false;
+//                     } else {
+//                         for (let item of this.blockList.children) {
+//                             const blockItemSpecification = item.querySelector(".block-item__specification");
+//                             const buttonIcon = item.querySelector(".button-icon");
+//                             if (blockItemSpecification.style.maxHeight) {
+//                                 blockItemSpecification.style.transition = "max-height 0.2s ease-out";
+//                                 blockItemSpecification.style.maxHeight = null;
+//                                 main.projectBlocks[buttonIcon.dataset.blockId].active = false;
+//                             }
+//                         }
+//                         blockItemSpecification.style.transition = "max-height 0.2s ease-out";
+//                         main.projectBlocks[buttonIcon.dataset.blockId].active = true;
+//                         blockItemSpecification.style.maxHeight = blockItemSpecification.scrollHeight + "px";
+//                     }
+//                 }
+//             }, true);
+
+//             if (main.projectBlocks[buttonIcon.dataset.blockId].active === true) {
+//                 blockItemSpecification.style.transition = "none";
+//                 blockItemSpecification.style.maxHeight = blockItemSpecification.scrollHeight + "px";
+//             }
+//         }
+//     },
+//     // Метод добавляет отступ справа для каждого игрового блока, при условии появления скролла
+//     scrollCheck(blockList) {
+//         // проверяем реальную ширину offset* элемента и фактическую client* (без учета ширины scroll-а)
+//         if (blockList.offsetWidth > blockList.clientWidth) {
+//             blockList.querySelectorAll('.block-item').forEach(item => {
+//                 item.style.marginRight = "4px";
+//             });
+//         }
+//     },
+// };
 
 main.launchApp();
